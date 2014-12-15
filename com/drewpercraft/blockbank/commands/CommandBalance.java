77b2d3165e84b0d1d088b05f751fa909f20bfe28
Jesse Jarrell <jjarrell@htsop.com>
2014-12-14 23:32:26 -0600

    Cleaned up the logger in commands

--- com/drewpercraft/blockbank/commands/CommandBalance.java
+++ com/drewpercraft/blockbank/commands/CommandBalance.java
@@ -1,5 +1,7 @@
 package com.drewpercraft.blockbank.commands;
 
+import java.util.logging.Logger;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,15 +11,17 @@ import com.drewpercraft.blockbank.BlockBank;
 public class CommandBalance implements CommandExecutor {
 
 	private final BlockBank plugin;
+	private final Logger log;
 	
 	public CommandBalance(BlockBank plugin) {
 		this.plugin = plugin;
+		this.log = this.plugin.getLogger();
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,	String[] arg3) {
 		// TODO Auto-generated method stub
-		this.plugin.getLogger().info("BlockBank: command Balance");
+		log.info(String.format("CommandBalance 1: %s  2: %s  3: %s", arg1, arg2, arg3));
 		return false;
 	}
 
