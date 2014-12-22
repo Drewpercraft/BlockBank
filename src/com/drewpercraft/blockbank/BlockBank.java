2a3a92a81fce3e6750f56df4220e7cb2caa15b40
hitechwizard <jjarrell@htsop.com>
2014-12-22 09:46:19 -0600

    formatted code sorting functions, etc.

--- src/com/drewpercraft/blockbank/BlockBank.java
+++ src/com/drewpercraft/blockbank/BlockBank.java
@@ -3,12 +3,12 @@ package com.drewpercraft.blockbank;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -17,7 +17,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.drewpercraft.blockbank.Bank;
 import com.drewpercraft.blockbank.commands.CommandBalance;
 import com.drewpercraft.blockbank.commands.CommandBalanceTop;
 import com.drewpercraft.blockbank.commands.CommandBank;
@@ -29,47 +28,113 @@ public final class BlockBank extends JavaPlugin {
 	
 	protected Logger log;
 	private Map<UUID, Bank> banks = new HashMap<UUID, Bank>();
-	private VaultEconomy vaultAPI = null;
+	private VaultEconomy vaultAPI = null;	
 	private ResourceBundle userMessages;
-	
-	@Override
-    public void onEnable() {
-		log = getLogger();
-    	
-    	if (loadConfiguration()) {
-        	log.info(String.format("Enabling %s commands", this.getName()));
-        	this.getCommand("bank").setExecutor(new CommandBank(this));
-        	this.getCommand("balance").setExecutor(new CommandBalance(this));
-        	this.getCommand("pay").setExecutor(new CommandPay(this));
-        	this.getCommand("balanceTop").setExecutor(new CommandBalanceTop(this));
-    	
-        	log.info(String.format("Enabling %s event handlers", this.getName()));
-        	getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
-	    	//TODO Start Security
-	    	//TODO Start InterestManager
-	    	/*
-			BukkitRunnable interestManager = new InterestManagerTask(this);
-			// Run the interestManager at the stroke of midnight (18000)
-			String worldName = getServer().getWorlds().get(0).getName();
-			long delay = 18000 - getServer().getWorld(worldName).getTime();
-			if (delay < 0) delay += 24000;
-			interestManager.runTaskTimer(this, delay, 24000);
-			getServer().getPluginManager().registerEvents(this, this);
-			log.info(String.format("%s has been enabled.", getDescription().getName()));	    	  
-	    	 
-	    	*/
-	    	log.info(String.format("%s enabled", this.getName()));
-    	}
-    }
  
-    @Override
-    public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
-    	log.info(String.format("%s Disabled", this.getName()));
+    public String getCurrencyPlural() {
+		return getConfig().getString("currencyPlural", "dollars");
+	}
+    
+    public String getCurrencySingular() {
+		return getConfig().getString("currencySingular", "dollar");
+	}
+    
+    public String getCurrencySymbol() {
+		return getConfig().getString("currencySymbol", "$");
+	}
+    
+	public int getDecimals() {
+		return getConfig().getInt("decimals", 2);
+	}
+
+	public boolean getDefaultAnnouncements() {
+		return getConfig().getBoolean("announcements", true);
+	}
+
+	public int getDefaultCloseHour() {
+		return getConfig().getInt("closeHour", 17);
+	}
+	
+	public double getDefaultLoanRate() {
+		return getConfig().getDouble("loanRate", 0);
+	}
+	
+	public int getDefaultMaxVaults() {
+		return getConfig().getInt("maxVaults", 10);
+	}
+	
+	public int getDefaultOpenHour() {
+		return getConfig().getInt("openHour", 8);
+	}
+    
+	public double getDefaultSavingsRate() {
+		return getConfig().getDouble("savingsRate", 0);
+	}
+	
+	public boolean getDropMoney() {
+		return getConfig().getBoolean("dropMoney", true);
+	}
+	
+	/*
+	 * Get the message using the key from the language file
+	 * with the config.yml dialogPrefix/dialogSuffix added
+	 */
+	public String getMessage(String key)
+	{
+		if (!userMessages.containsKey(key)) {
+			log.severe(String.format("Language file is missing the %s key", key));
+			return key;
+		}
+		return getConfig().getString("dialogPrefix", "") + userMessages.getString(key) + getConfig().getString("dialogSuffix", "");
+	}
+	
+	/*
+	 * Get the message using the key from the language file
+	 * and format the message using the params provided
+	 */
+	public String getMessage(String key, Object... args)
+	{
+		return String.format(getMessage(key), args);
+	}
+	
+	/*
+	 * Returns the bank owning the branch or ATM the player is standing
+	 * in or null if he is not in a bank
+	 */
+	public Bank getPlayerBank(Player player) 
+	{
+		// TODO
+		return null;
+	}
+	
+	/*
+	 * Returns the bank owning the branch or ATM the player is standing
+	 * in or null if he is not in a bank
+	 */
+	public Branch getPlayerBranch(Player player)
+	{
+		// TODO
+		return null;
+	}
+
+	/*
+	 * Get the full path to where the player data files are stored
+	 */
+	public String getPlayerDataPath() {
+		String filePath = getConfig().getString("playerDataPath", null);
+		if (filePath == null) {
+			filePath = getDataFolder().getAbsolutePath() + File.separator + "playerData";
+		}
+		return filePath;
+	}
+	
+	public VaultEconomy getVaultAPI()
+    {
+    	if (vaultAPI == null) vaultAPI = new VaultEconomy(this);
+    	return vaultAPI;
     }
-    
-    
-    public boolean loadConfiguration() {
+	
+	public boolean loadConfiguration() {
     	log.info("Loading configuration");
     	
     	try {
@@ -100,7 +165,12 @@ public final class BlockBank extends JavaPlugin {
 	    	 
 			try {
 			  userMessages = new PropertyResourceBundle(fis);
-			} finally {
+			} 
+			catch (Exception e) {
+				log.severe("Error loading language file:");
+				throw e;
+			}
+			finally {
 			  fis.close();
 			}
 	    	
@@ -126,93 +196,51 @@ public final class BlockBank extends JavaPlugin {
     	}
     	return true;
     }
-    
-    public VaultEconomy getVaultAPI()
-    {
-    	if (vaultAPI == null) vaultAPI = new VaultEconomy(this);
-    	return vaultAPI;
+
+	@Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    	log.info(String.format("%s Disabled", this.getName()));
     }
-    
-    
-	
-	protected static boolean booleanValue(String value) {		
-		if (Integer.parseInt("0" + value) > 0 || value.equals("on") || value.startsWith("y") || value.startsWith("t")) return true;
-		return false;
-	}
 
-	public int getDefaultMaxVaults() {
-		return getConfig().getInt("maxVaults", 10);
-	}
+	@Override
+    public void onEnable() {
+		log = getLogger();
+    	
+    	if (loadConfiguration()) {
+        	log.info(String.format("Enabling %s commands", this.getName()));
+        	this.getCommand("bank").setExecutor(new CommandBank(this));
+        	this.getCommand("balance").setExecutor(new CommandBalance(this));
+        	this.getCommand("pay").setExecutor(new CommandPay(this));
+        	this.getCommand("balanceTop").setExecutor(new CommandBalanceTop(this));
+    	
+        	log.info(String.format("Enabling %s event handlers", this.getName()));
+        	getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+	    	//TODO Start Security
+	    	//TODO Start InterestManager
+        	//TODO Start Background data file saver
+	    	/*
+			BukkitRunnable interestManager = new InterestManagerTask(this);
+			// Run the interestManager at the stroke of midnight (18000)
+			String worldName = getServer().getWorlds().get(0).getName();
+			long delay = 18000 - getServer().getWorld(worldName).getTime();
+			if (delay < 0) delay += 24000;
+			interestManager.runTaskTimer(this, delay, 24000);
+			getServer().getPluginManager().registerEvents(this, this);
+			log.info(String.format("%s has been enabled.", getDescription().getName()));	    	  
+	    	 
+	    	*/
+	    	log.info(String.format("%s enabled", this.getName()));
+    	}
+    }
 
-	public boolean getDefaultAnnouncements() {
-		return getConfig().getBoolean("announcements", true);
-	}
-	
-	public double getDefaultSavingsRate() {
-		return getConfig().getDouble("savingsRate", 0);
-	}
-	
-	public double getDefaultLoanRate() {
-		return getConfig().getDouble("loanRate", 0);
-	}
-	
-	public int getDefaultOpenHour() {
-		return getConfig().getInt("openHour", 8);
-	}
-    
-	public int getDefaultCloseHour() {
-		return getConfig().getInt("closeHour", 17);
-	}
-	
-	public boolean getDropMoney() {
-		return getConfig().getBoolean("dropMoney", true);
-	}
-	
-	public String getCurrencyPlural() {
-		return getConfig().getString("currencyPlural", "dollars");
-	}
-	
-	public String getCurrencySingular() {
-		return getConfig().getString("currencySingular", "dollar");
-	}
-	
-	public String getCurrencySymbol() {
-		return getConfig().getString("currencySymbol", "$");
-	}
-	
-	public int getDecimals() {
-		return getConfig().getInt("decimals", 2);
-	}
-
-	public String getPlayerDataPath() {
-		String filePath = getConfig().getString("playerDataPath", null);
-		if (filePath == null) {
-			filePath = getDataFolder().getAbsolutePath() + File.separator + "playerData";
-		}
-		return filePath;
-	}
-	
-	public String getMessage(String key)
+	/*
+	 *  Send the player a message based on the language file
+	 */
+	public void sendMessage(CommandSender player, String key, Object... args) 
 	{
-		if (!userMessages.containsKey(key)) {
-			log.severe(String.format("Language file is missing the %s key", key));
-			return key;
-		}
-		return getConfig().getString("dialogPrefix", "") + userMessages.getString(key) + getConfig().getString("dialogSuffix", "");
-	}
-	
-	public String getMessage(String key, Object... args)
-	{
-		return String.format(getMessage(key), args);
-	}
-
-	public void sendMessage(CommandSender player, String key, Object... args) {
 		player.sendMessage(getMessage(key, args));		
 	}
-
-
-
-	
 
 	
 }
